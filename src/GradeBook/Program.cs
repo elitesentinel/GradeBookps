65bf1630cbab01af7415971152379e8f1fc3d138
Arslan Recep Aykök <recepaykok@gmail.com>
2022-04-08 15:55:18 +0300

    Added my project

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GradeBook;
+class Program
+{
+    static void Main(String[] args)
+    {
+        var book = new Book("Arslan's Grade Book");
+        book.AddGrade(89.1);
+        book.AddGrade(90.5);
+        book.AddGrade(77.3);
+
+        var stats = book.GetStatistics();
+        Console.WriteLine($"Highest grade is: {stats.High}, lowest grade is: {stats.Low}, average grade is: {stats.Average}");
+    }
+}
+
+
